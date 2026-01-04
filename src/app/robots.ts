5825200eba2fbf692a60c190c09cebeb1e2d5092
William <william@marksendigital.se>
2026-01-04 21:07:17 +0100

    fix: update robots.ts with granular _next/ blocking and CSS exception

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,9 +4,10 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/', '/static/'],
+      allow: ['/', '/_next/static/css/'],
+      disallow: ['/api/', '/_next/static/chunks/', '/_next/static/media/', '/_next/image/', '/static/'],
     },
     sitemap: 'https://www.convextraredovisning.se/sitemap.xml',
   };
 }
+
